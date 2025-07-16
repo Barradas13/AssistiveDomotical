9b1982e440ed9137bd568a6120c51670069fd2f1
Felipe Barradas Sebastião <febarradas13@gmail.com>
2025-07-16 18:15:12 -0300

    Add files via upload

---
+++ explore.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("../tabelas/total.mp4DLIB.csv")
+
+df = df[df['timestamp'] < 30.0]
+
+cores = df['piscando'].map({'vermelho': 'red', 'verde': 'green'})
+
+plt.figure(figsize=(14, 6))
+plt.scatter(df['timestamp'], df['ear'], c=cores, s=10, alpha=0.8)
+
+
+plt.title('EAR vs Tempo (com piscando em cores) DLIB')
+plt.xlabel('Tempo (s)')
+plt.ylabel('EAR')
+plt.grid(True)
+plt.tight_layout()
+
+
+plt.show()
